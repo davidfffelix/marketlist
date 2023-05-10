7cb768a0af0bae111df4d5942d5f5c88f6502037
David Felix <davidffelix@hotmail.com>
2023-05-10 09:54:38 -0300

    Insert data input

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // final List _shopping = List[];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -16,6 +18,34 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.green,
           title: const Text('MarketList'),
           leading: const Icon(Icons.menu),
+        ),
+        body: Column(
+          children: const [
+            Text('Nome do Produto: '),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Inserir Nome',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Preço do Produto'),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Adicionar Preço',
+              ),
+            ),
+            // ListView.builder(
+            //   shrinkWrap: true,
+            //   itemCount: 10,
+            //   itemBuilder: (context, index) {
+            //     return const ListTile(
+            //       title: Text('Produto'),
+            //     );
+            //   },
+            // ),
+          ],
         ),
       ),
     );
---
+++ lib/models/product.dart
@@ -0,0 +1,8 @@
+class Product {
+  final String name;
+  final double value;
+  Product({
+    required this.name,
+    required this.value,
+  });
+}

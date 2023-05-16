96120c806f784c98196444e4bbe25c9e35babd1e
David Felix <davidffelix@hotmail.com>
2023-05-16 14:29:00 -0300

    Insert to sort list

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -4,14 +4,18 @@ import 'models/product_model.dart';
 
 class HomeController extends GetxController {
   List<ProductModel> products = [
-    ProductModel(name: 'farinha', price: 20),
-    ProductModel(name: 'arroz', price: 12),
-    ProductModel(name: 'feijão', price: 15),
-    ProductModel(name: 'óleo', price: 5),
+    ProductModel(name: 'Farinha', price: 20),
+    ProductModel(name: 'Arroz', price: 12),
+    ProductModel(name: 'Feijão', price: 15),
+    ProductModel(name: 'Óleo', price: 5),
+    ProductModel(name: 'Macarrão', price: 2),
+    ProductModel(name: 'Café', price: 8),
   ];
 
   final nameTextEditController = TextEditingController();
   final priceTextEditController = TextEditingController();
+
+  bool isSorted = false;
   // var sortType = sortLowerPrice();
 
   // late ProductModel productModel;
@@ -25,22 +29,27 @@ class HomeController extends GetxController {
   }
 
   void sortLowerPrice() {
-    products.sort(
-      (a, b) {
-        return a.price.compareTo(b.price);
-      },
-    );
+    if (!isSorted) {
+      products.sort(
+        (a, b) {
+          return b.price.compareTo(a.price);
+        },
+      );
+      isSorted = true;
+    } else {
+      products.reversed.toList();
+    }
     update();
   }
 
-  void sortHigherPrice() {
-    products.sort(
-      (a, b) {
-        return b.price.compareTo(a.price);
-      },
-    );
-    update();
-  }
+  // void sortHigherPrice() {
+  //   products.sort(
+  //     (a, b) {
+  //       return b.price.compareTo(a.price);
+  //     },
+  //   );
+  //   update();
+  // }
 
   void addProducts(String name, double price) {
     products.add(ProductModel(name: name, price: price));
